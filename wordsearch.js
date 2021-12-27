e721b205cef02ef1b992bb2df534906cae083504
DuyguY <duygu.yldrm4820@gmail.com>
2021-12-27 18:37:44 -0500

    Check to find the word horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
+// const wordSearch = (letters, word) => { 
+//   const horizontalJoin = letters.map(ls => ls.join(''));
+//   const verticalJoin = ['','','','','','','',''];
+
+//   for (let i = 0; i < horizontalJoin.length; i++) {
+//     for (let j = 0; j < horizontalJoin[i].length; j++) {
+//       verticalJoin[j] += horizontalJoin[i][j];
+//     }
+//   }
+//   for (let l of horizontalJoin) {
+//     if (l.includes(word)) return true;
+//   }
+//   for (let l of verticalJoin) {
+//     if (l.includes(word)) return true;
+//   }
+//   return false;
+// };
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+  const verticalJoin = ['','','','','','','',''];
+  for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+      }
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (const l of horizontalJoin) {
+      if (l === word) {
+          return true;
+      }
+  }
+  return false;
+};
 
 module.exports = wordSearch
